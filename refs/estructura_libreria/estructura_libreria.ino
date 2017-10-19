786c0bd9f6da267d8d034dee9300f411f970b4dd
baruc <baruc.almaguer@gmail.com>
2017-10-19 11:43:19 -0500

    commit inicial y referencias (taskScheduler)

---
+++ refs/estructura_libreria/estructura_libreria.ino
@@ -0,0 +1,39 @@
+
+//Facil
+void getPixel(){
+  //WHOAMI pendiente
+  int x = parser.getInt();
+  int y = parser.getInt();
+
+  float temp = camara.readPixel(x, y);
+
+  parser.response(cmdPix, temp);
+}
+
+//Ideal
+void getPixel(int x, int y){
+  float temp = camara.readPixel(x, y);
+  return temp;
+}
+
+//Simple
+Command cmdPix("pix", getPixel);
+//Ideal
+Command cmdPix("pix", getPixel, int, int, float);
+
+
+CommandParser parser(9600, P_P, NONE);
+CommandParser parser(115200, P_P, CHECKSUM);
+CommandParser parser(9600, BUS, CRC);
+CommandParser parser(9600, st='$',del=',', fn='\n', P_P, NONE);
+
+
+void setup() {
+  parser.addcmd(cmdPix);
+  parser.name("INO_IR");
+  parser.begin();
+}
+
+void loop() {
+  parser.parse(); //Do your thing!
+}
